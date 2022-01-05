8f377be5845537f84bc66cdc9cd6fea08d3caef8
sklecka <94263845+sklecka@users.noreply.github.com>
2022-01-04 22:18:06 -0500

    fixed bug in player name accepting null

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,9 +69,21 @@ var fight = function(enemy) {
   }
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 // player info
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
